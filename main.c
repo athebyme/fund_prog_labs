95f792b69597765294098213c998f1d873856c31
athebyme <367932@edu.itmo.ru>
2022-09-14 11:03:33 +0300

    lab1-2

--- main.c
+++ main.c
@@ -3,28 +3,23 @@
 #include <math.h>
 
 int lab1(){
-    system("chcp 1251");
-    printf("\n-----------------\n");
-    printf("\n\nЛабораторная работа 1\n");
+    printf("\n-----------------\n\n\nLab 1\n");
     int _int;
     float _f;
-    printf("Введите целое число: ");
+    printf("Enter int num: ");
     scanf("%d", &_int);
-    printf("Вы ввели число: %d",_int);
-    printf("\nВведите вещественное число: ");
+    printf("You entered %d num",_int);
+    printf("\nEnter float num: ");
     scanf("%f",&_f);
-    printf("Вы ввели число %.2f", _f);
+    printf("You entered %.2f num", _f);
     return 0;
 }
 
 int lab2(){
-    system("chcp 1251");
-    printf("\n-----------------\n");
-    printf("\n\nLab 2\n");
-    float a;
+    printf("\n-----------------\n\n\nLab 2\n");
+    float a, z1, z2;
     printf("Введите вещественное число: ");
     scanf("%f", &a);
-    float z1, z2;
     z1 = cosf(a) + sinf(a) + cosf(3*a) + sinf(3*a);
     printf("res z1: %f", z1);
     z2 = 2 * sqrtf(2) * cosf(a) * sinf(M_PI/4 + 2 * a);
@@ -32,6 +27,7 @@ int lab2(){
     return 0;
 }
 int main() {
+    system("chcp 1251");
     lab1();
     lab2();
 }
