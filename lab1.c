21212e561541839ba7191045db4e034f45c2492f
athebyme <367932@edu.itmo.ru>
2022-09-14 20:37:14 +0300

    lab1-2

---
+++ lab1.c
@@ -0,0 +1,15 @@
+#include "lab1.h"
+#include <stdio.h>
+
+int lab1(){
+    printf("\n-----------------\n\n\nLab 1\n");
+    int _int;
+    float _f;
+    printf("Enter int num: ");
+    scanf("%d", &_int);
+    printf("You entered %d num",_int);
+    printf("\nEnter real num: ");
+    scanf("%f",&_f);
+    printf("You entered %.2f num", _f);
+    return 0;
+}
---
+++ lab1.h
@@ -0,0 +1,8 @@
+//
+// Created by Anton on 14.09.2022.
+//
+
+#ifndef UNTITLED_LAB1_H
+#define UNTITLED_LAB1_H
+int lab1();
+#endif //UNTITLED_LAB1_H
