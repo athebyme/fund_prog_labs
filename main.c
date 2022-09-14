232a3745b723c043b9b719142263efe3a6b44ab8
athebyme <367932@edu.itmo.ru>
2022-09-14 10:57:32 +0300

    lab1-2

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int lab1(){
+    printf("\n-----------------\n");
+    printf("\n\nЛабораторная работа 1\n");
+    int _int;
+    float _f;
+    printf("Введите целое число: ");
+    scanf("%d", &_int);
+    printf("Вы ввели число: %d",_int);
+    printf("\nВведите вещественное число: ");
+    scanf("%f",&_f);
+    printf("Вы ввели число %.2f", _f);
+    return 0;
+}
+
+int lab2(){
+    printf("\n-----------------\n");
+    printf("\n\nЛабораторная работа 2\n");
+    float a;
+    printf("Введите число: ");
+    scanf("%f", &a);
+    float z1, z2;
+    z1 = cosf(a) + sinf(a) + cosf(3*a) + sinf(3*a);
+    printf("res z1: %f", z1);
+    z2 = 2 * sqrtf(2) * cosf(a) * sinf(M_PI/4 + 2 * a);
+    printf("\nres z2: %f", z2);
+    return 0;
+}
+int main() {
+    system("chcp 1251");
+    lab1();
+    lab2();
+}
